aa74c0c16de5215b69df8aa7cfce5bb7b16ada2e
seedsat <e.satoshi.bi@gmail.com>
2021-06-14 19:52:26 +0900

    投稿機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,5 +8,17 @@ class PicturesController < ApplicationController
   end
 
   def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:name, :info, :member_name, :group_id, :status_id, :position_id, images: []).merge(user_id: current_user.id)
   end
 end
