d3d5b47bf470f651c9d346a51cbaeffbf2af3ec1
seedsat <e.satoshi.bi@gmail.com>
2021-06-15 15:19:21 +0900

    投稿編集機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :find_picture, only: [:edit, :update]
   
   def index
     @pictures = Picture.includes(:user).order('created_at desc')
@@ -17,9 +18,24 @@ class PicturesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @picture.update(picture_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def picture_params
     params.require(:picture).permit(:name, :info, :member_name, :group_id, :status_id, :position_id, images: []).merge(user_id: current_user.id)
+  end
+
+  def find_picture
+    @picture = Picture.find(params[:id])
   end
 end
