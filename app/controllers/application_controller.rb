718cdb55b4f16e08ed40adad8d45bd1056d11629
seedsat <e.satoshi.bi@gmail.com>
2021-06-16 15:04:23 +0900

    マイページの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [
                                       :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana
                                       ])
+    devise_parameter_sanitizer.permit(:account_update,keys: [
+                                      :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana
+                                      ])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'pictures#index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+  }
   resources :pictures
   resources :users, only: [:show]
 end
