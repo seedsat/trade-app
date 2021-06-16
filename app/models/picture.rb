c49c4b7d8c181ef152535f6e92bb8848b1919c00
seedsat <e.satoshi.bi@gmail.com>
2021-06-16 17:37:45 +0900

    アソシエーション とバリデーションを定義

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,7 @@
 class Picture < ApplicationRecord
   belongs_to :user
   has_many_attached :images, dependent: :destroy
+  has_many :comments
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :group
