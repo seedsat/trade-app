af1028aa06ce747ba8451acf38c8b0afa0f3d180
seedsat <e.satoshi.bi@gmail.com>
2021-06-16 19:58:32 +0900

    コメント機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,15 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to picture_path(params[:picture_id])
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, picture_id: params[:picture_id])
+  end
 end
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_picture, only: [:edit, :update, :destroy, :show]
   before_action :move_index, only: [:edit, :update, :destroy]
 
@@ -36,6 +36,8 @@ class PicturesController < ApplicationController
   end
 
   def show
+    @comment = Comment.new
+    @comments = @picture.comments.includes(:user)
   end
 
   private
