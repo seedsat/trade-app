95dc38f053005f97a2442e23f0afc9d2e97cf9b9
seedsat <e.satoshi.bi@gmail.com>
2021-06-17 14:24:14 +0900

    非ログインでも検索可

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :find_picture, only: [:edit, :update, :destroy, :show]
   before_action :move_index, only: [:edit, :update, :destroy]
   before_action :search_pictures, only: [:index, :show, :search]
