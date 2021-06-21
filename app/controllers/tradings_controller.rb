2f2558dcd20cfcf705728b13714aa097056a7c26
seedsat <e.satoshi.bi@gmail.com>
2021-06-21 13:10:31 +0900

    ルーティングとコントローラーの設定

---
+++ app/controllers/tradings_controller.rb
@@ -0,0 +1,5 @@
+class TradingsController < ApplicationController
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
   }
   resources :pictures do
+    resources :tradings, only: :create
     resources :comments, only: :create
     collection do
       get 'search'
